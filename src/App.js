cd5b27acbfd83e66416b56c0690174fcc199cfca
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-03-09 00:04:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import FooterSection from './components/Footer';
+import Navbar from './components/Navbar';
+import Quotes from './components/Quotes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Quotes></Quotes>
+      <FooterSection></FooterSection>
     </div>
   );
 }
